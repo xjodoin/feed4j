a6abd2f0db55aec66b71c28f72112f71949864fb
icyleaf <icyleaf.cn@gmail.com>
2010-02-26 12:32:31 +0800

    Added 'isChinese' static method in HTMLEntities class.

--- src/it/sauronsoftware/feed4j/html/HTMLEntities.java
+++ src/it/sauronsoftware/feed4j/html/HTMLEntities.java
@@ -597,9 +597,9 @@ class HTMLEntities {
 				buffer.append(";");
 			} else {
 				int ci = 0xffff & c;
-				if (ci < 160)
+				if (ci < 160 || isChinese(c)) {
 					buffer.append(c);
-				else {
+				} else {	
 					buffer.append("&#");
 					buffer.append(ci);
 					buffer.append(';');
@@ -608,5 +608,18 @@ class HTMLEntities {
 		}
 		return buffer.toString();
 	}
+	
+	public static boolean isChinese(char c) {
+		Character.UnicodeBlock ub = Character.UnicodeBlock.of(c);
+		if (ub == Character.UnicodeBlock.CJK_UNIFIED_IDEOGRAPHS
+				|| ub == Character.UnicodeBlock.CJK_COMPATIBILITY_IDEOGRAPHS
+				|| ub == Character.UnicodeBlock.CJK_UNIFIED_IDEOGRAPHS_EXTENSION_A
+				|| ub == Character.UnicodeBlock.GENERAL_PUNCTUATION
+				|| ub == Character.UnicodeBlock.CJK_SYMBOLS_AND_PUNCTUATION
+				|| ub == Character.UnicodeBlock.HALFWIDTH_AND_FULLWIDTH_FORMS) {
+			return true;
+		}
+		return false;
+	} 
 
 }
